178ac432d4baf545c813ed0c8bda8e3ac8fbf2b5
Mark Watney <me@mwatney.com>
2020-04-07 10:51:37 +0000

    Original Release

--- yaki.sh
+++ yaki.sh
@@ -1,35 +1,59 @@
 #!/bin/bash
 
-#KUBERNETES_VERSION ## If you want to use a different version of kubernetes, change it on install_prereqs.sh 
-INSTANCE_NAME_PREFIX="kubeadm-test"
+KUBERNETES_VERSION="1.17.4-00"
+INSTANCE_NAME_SUFIX="kubeadm-test"
+INSTANCE_NAME_PREFIX="yaki_"
 INSTANCE_IMAGE="debian-9-drawfork-v20200207"
 INSTANCE_MACHINE_TYPE="n1-standard-2"
 GCLOUD_ZONE="us-central1-a"
 QTD_NODES="1"
-STARTUP_SCRIPT="startup-script/install_prereqs.sh"
+STARTUP_SCRIPT_PATH="startup-script/"
+STARTUP_SCRIPT="install_prereqs.sh"
 CALICO_MANIFEST="https://docs.projectcalico.org/v3.11/manifests/calico.yaml"
 DELETE_OLD_CLUSTER="yes"
 LOGFILE="./log/auto_kubeadm.log"
 
-echo "Adding your ssh-key to the authentication agent"
-echo " "
-eval `ssh-agent -s` > /dev/null 2>&1
-ssh-add ~/.ssh/google_compute_engine
+function define_instances_name {
+  INSTANCE_NAME="$INSTANCE_NAME_PREFIX""$INSTANCE_NAME_SUFIX"
+}
 
-if [ $? -eq 0 ]; then
-  echo ""
-else
+function prep_startup_script {
+  sed "2 aKUBERNETES_VERSION=\"$KUBERNETES_VERSION\"" "$STARTUP_SCRIPT_PATH"base > "$STARTUP_SCRIPT_PATH""$STARTUP_SCRIPT"
+}
+
+function quit {
+  exit
+}
+
+function add_ssh_key {
+  echo "Adding your ssh-key to the authentication agent"
   echo " "
-  echo "Ops, failed to add your ssh-key to the authentication agent. Check your passphrase."
-  exit 1
-fi
+  eval `ssh-agent -s` > /dev/null 2>&1
+  ssh-add ~/.ssh/google_compute_engine
 
-mkdir ./log > /dev/null 2>&1
-clear
+  if [ $? -eq 0 ]; then
+    echo ""
+  else
+    echo " "
+    echo "Ops, failed to add your ssh-key to the authentication agent. Check your passphrase."
+    quit
+  fi
+}
+
+function setup_logs {
+  mkdir ./log > /dev/null 2>&1
+  clear
+}
+
+define_instances_name
+prep_startup_script
+add_ssh_key
+setup_logs
 
 echo "Your cluster will be created using the following variables:"
 echo " "
-echo "INSTANCE_NAME_PREFIX=$INSTANCE_NAME_PREFIX
+echo "KUBERNETES_VERSION=$KUBERNETES_VERSION
+INSTANCE_NAME=$INSTANCE_NAME
 INSTANCE_IMAGE=$INSTANCE_IMAGE
 INSTANCE_MACHINE_TYPE=$INSTANCE_MACHINE_TYPE
 GCLOUD_ZONE=$GCLOUD_ZONE
@@ -49,7 +73,7 @@ echo " "
 if [ "$DELETE_OLD_CLUSTER" == "yes" ]; then
   echo "Checking if there are existent compute instances that need to be removed"
   echo " "
-  INSTANCES_LIST=$(gcloud compute instances list --filter="$INSTANCE_NAME_PREFIX" --format='value(NAME)')
+  INSTANCES_LIST=$(gcloud compute instances list --filter="$INSTANCE_NAME" --format='value(NAME)')
   if [ -z "$INSTANCES_LIST" ]; then
     echo "Nothing to remove"
     echo " "
@@ -60,15 +84,15 @@ if [ "$DELETE_OLD_CLUSTER" == "yes" ]; then
     echo " " 
     echo "Removing existend instances (it may take a while)"
     echo " "
-    gcloud compute instances list --filter="$INSTANCE_NAME_PREFIX" --format='value(NAME)' | xargs -I {} gcloud compute instances delete {} --zone $GCLOUD_ZONE --quiet
+    gcloud compute instances list --filter="$INSTANCE_NAME" --format='value(NAME)' | xargs -I {} gcloud compute instances delete {} --zone $GCLOUD_ZONE --quiet
     sleep 10
   fi
 else
   i=1
   while [ $i -lt "$QTD_NODES" ]; do
-    INSTANCE_CHECK=$(gcloud compute instances list | awk '{ print $1 }' | grep "$INSTANCE_NAME_PREFIX-${i}")
-    if [ "$INSTANCE_CHECK" == "$INSTANCE_NAME_PREFIX-${i}" ]; then
-      echo "ERROR: Oh I can't proceed. have compute instances using the same prefix defined on INSTANCE_NAME_PREFIX ($INSTANCE_NAME_PREFIX). 
+    INSTANCE_CHECK=$(gcloud compute instances list | awk '{ print $1 }' | grep "$INSTANCE_NAME-${i}")
+    if [ "$INSTANCE_CHECK" == "$INSTANCE_NAME-${i}" ]; then
+      echo "ERROR: Oh I can't proceed. have compute instances using the same prefix defined on INSTANCE_NAME ($INSTANCE_NAME). 
        Chose a different prefix or change DELETE_OLD_CLUSTER to yes if you allow me to delete it (DELETE_OLD_CLUSTER = $DELETE_OLD_CLUSTER)"
       exit 1
     fi
@@ -80,7 +104,7 @@ echo "Creating new compute instances"
 echo " "
 i=1
 while [ $i -le "$QTD_NODES" ]; do
-  gcloud compute instances create $INSTANCE_NAME_PREFIX-${i} \
+  gcloud compute instances create $INSTANCE_NAME-${i} \
     --async \
     --boot-disk-size 100GB \
     --boot-disk-type=pd-ssd \
@@ -91,7 +115,7 @@ while [ $i -le "$QTD_NODES" ]; do
     --scopes compute-rw,storage-ro,service-management,service-control,logging-write,monitoring \
     --zone $GCLOUD_ZONE \
     --metadata-from-file startup-script=$STARTUP_SCRIPT  > $LOGFILE 2>&1 &
-  echo "$INSTANCE_NAME_PREFIX-${i} created"
+  echo "$INSTANCE_NAME-${i} created"
   i=$((i + 1))
 done
 
@@ -103,18 +127,18 @@ echo " "
 i=1
 READINESS=false
 while [ $READINESS != true ]; do
-  VAR=$(gcloud compute ssh $INSTANCE_NAME_PREFIX-1 --zone $GCLOUD_ZONE --command "grep PREREQSDONE /var/log/daemon.log" 2> /dev/null)
+  VAR=$(gcloud compute ssh $INSTANCE_NAME-1 --zone $GCLOUD_ZONE --command "grep PREREQSDONE /var/log/daemon.log" 2> /dev/null)
   if [ -n "$VAR" ]; then 
     echo " "
-    echo "OK! Master Node ($INSTANCE_NAME_PREFIX-1) is Ready"
+    echo "OK! Master Node ($INSTANCE_NAME-1) is Ready"
     echo " "
 
     ### Adding user to docker group 
-    gcloud compute ssh $INSTANCE_NAME_PREFIX-1 --zone $GCLOUD_ZONE --command "sudo groupadd docker; sudo usermod -aG docker $USER" > $LOGFILE 2>&1
+    gcloud compute ssh $INSTANCE_NAME-1 --zone $GCLOUD_ZONE --command "sudo groupadd docker; sudo usermod -aG docker $USER" > $LOGFILE 2>&1
 
     READINESS=true
   else 
-    echo "$INSTANCE_NAME_PREFIX-1 Not Ready..."
+    echo "$INSTANCE_NAME-1 Not Ready..."
     sleep 15
   fi
 done
@@ -122,23 +146,23 @@ done
 ### Initialize cluster on node 0
 echo "Initializing Master Node"
 echo " "
-gcloud compute ssh $INSTANCE_NAME_PREFIX-1 --zone $GCLOUD_ZONE --command "sudo kubeadm init --pod-network-cidr=192.168.0.0/16" > $LOGFILE 2>&1
+gcloud compute ssh $INSTANCE_NAME-1 --zone $GCLOUD_ZONE --command "sudo kubeadm init --pod-network-cidr=192.168.0.0/16" > $LOGFILE 2>&1
 
-gcloud compute ssh $INSTANCE_NAME_PREFIX-1 --zone $GCLOUD_ZONE --command "mkdir -p $HOME/.kube; sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config; sudo chown $(id -u):$(id -g) $HOME/.kube/config; sudo chown $USER:$USER -R .kube/" 
+gcloud compute ssh $INSTANCE_NAME-1 --zone $GCLOUD_ZONE --command "mkdir -p $HOME/.kube; sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config; sudo chown $(id -u):$(id -g) $HOME/.kube/config; sudo chown $USER:$USER -R .kube/" 
 
 ### Enabling Auto-Completion for kubectl 
-gcloud compute ssh $INSTANCE_NAME_PREFIX-1 --zone $GCLOUD_ZONE --command "source /usr/share/bash-completion/bash_completion" 
+gcloud compute ssh $INSTANCE_NAME-1 --zone $GCLOUD_ZONE --command "source /usr/share/bash-completion/bash_completion" 
 
 echo "Applying Callico CNI"
 echo " "
-gcloud compute ssh $INSTANCE_NAME_PREFIX-1 --zone $GCLOUD_ZONE --command "kubectl apply -f $CALICO_MANIFEST" > $LOGFILE 2>&1
+gcloud compute ssh $INSTANCE_NAME-1 --zone $GCLOUD_ZONE --command "kubectl apply -f $CALICO_MANIFEST" > $LOGFILE 2>&1
 
 ### Enable ipip communication for calico ### GCE blocks traffic between hosts by default; the following command allow Calico traffic to flow between containers on different hosts. 
 gcloud compute firewall-rules create calico-ipip --allow 4 --network "default" --source-ranges "10.128.0.0/9" > $LOGFILE 2>&1
 
-MASTER_STATUS=$(gcloud compute ssh "$INSTANCE_NAME_PREFIX-1" --zone "$GCLOUD_ZONE" --command "kubectl get nodes" | grep master | awk '{ print $2 }')
+MASTER_STATUS=$(gcloud compute ssh "$INSTANCE_NAME-1" --zone "$GCLOUD_ZONE" --command "kubectl get nodes" | grep master | awk '{ print $2 }')
 while [ "$MASTER_STATUS" != "Ready" ]; do
-  MASTER_STATUS=$(gcloud compute ssh "$INSTANCE_NAME_PREFIX-1" --zone "$GCLOUD_ZONE" --command "kubectl get nodes" | grep master | awk '{ print $2 }')
+  MASTER_STATUS=$(gcloud compute ssh "$INSTANCE_NAME-1" --zone "$GCLOUD_ZONE" --command "kubectl get nodes" | grep master | awk '{ print $2 }')
   echo "Waiting cluster to get Ready (Status: "$MASTER_STATUS")"
 
   sleep 2
@@ -146,20 +170,20 @@ done
 echo " "
 
 if [ "$QTD_NODES" -gt 1 ]; then
-  gcloud compute ssh "$INSTANCE_NAME_PREFIX-1" --zone "$GCLOUD_ZONE" --command "kubeadm token create --print-join-command > joincmd" > $LOGFILE 2>&1
+  gcloud compute ssh "$INSTANCE_NAME-1" --zone "$GCLOUD_ZONE" --command "kubeadm token create --print-join-command > joincmd" > $LOGFILE 2>&1
   x=2
   while [ "$x" -le "$QTD_NODES" ]; do
-    gcloud compute scp "$INSTANCE_NAME_PREFIX-1:~/joincmd" --zone "$GCLOUD_ZONE" . > $LOGFILE 2>&1 
-    gcloud compute scp ./joincmd "$INSTANCE_NAME_PREFIX-$x:~/joincmd" --zone "$GCLOUD_ZONE"  > $LOGFILE 2>&1
-    echo "Joining Slave Node: $INSTANCE_NAME_PREFIX-$x"
-    gcloud compute ssh "$INSTANCE_NAME_PREFIX-$x" --zone "$GCLOUD_ZONE" --command "sudo sh ./joincmd" > $LOGFILE 2>&1
-    gcloud compute ssh "$INSTANCE_NAME_PREFIX-$x" --zone "$GCLOUD_ZONE" --command "rm ./joincmd" > $LOGFILE 2>&1
+    gcloud compute scp "$INSTANCE_NAME-1:~/joincmd" --zone "$GCLOUD_ZONE" . > $LOGFILE 2>&1 
+    gcloud compute scp ./joincmd "$INSTANCE_NAME-$x:~/joincmd" --zone "$GCLOUD_ZONE"  > $LOGFILE 2>&1
+    echo "Joining Slave Node: $INSTANCE_NAME-$x"
+    gcloud compute ssh "$INSTANCE_NAME-$x" --zone "$GCLOUD_ZONE" --command "sudo sh ./joincmd" > $LOGFILE 2>&1
+    gcloud compute ssh "$INSTANCE_NAME-$x" --zone "$GCLOUD_ZONE" --command "rm ./joincmd" > $LOGFILE 2>&1
 
     x=$((x + 1))
   done
   x=$((x - 1))
   while [ "$LASTNODE_STATUS" != "Ready" ]; do
-    LASTNODE_STATUS=$(gcloud compute ssh "$INSTANCE_NAME_PREFIX-1" --zone "$GCLOUD_ZONE" --command "kubectl get nodes" | grep $INSTANCE_NAME_PREFIX-$x | awk '{ print $2 }')
+    LASTNODE_STATUS=$(gcloud compute ssh "$INSTANCE_NAME-1" --zone "$GCLOUD_ZONE" --command "kubectl get nodes" | grep $INSTANCE_NAME-$x | awk '{ print $2 }')
     echo " "
     echo "Waiting last node to get Ready (Status: $LASTNODE_STATUS)"
 
@@ -168,15 +192,15 @@ if [ "$QTD_NODES" -gt 1 ]; then
 else
   echo "Allowing pods to be scheduled on the control-plane node"
   echo " "
-  gcloud compute ssh "$INSTANCE_NAME_PREFIX-1" --zone "$GCLOUD_ZONE" --command "kubectl taint nodes --all node-role.kubernetes.io/master-"
+  gcloud compute ssh "$INSTANCE_NAME-1" --zone "$GCLOUD_ZONE" --command "kubectl taint nodes --all node-role.kubernetes.io/master-"
 fi
 rm ./joincmd > $LOGFILE 2>&1
 
-gcloud compute ssh "$INSTANCE_NAME_PREFIX-1" --zone "$GCLOUD_ZONE" --command "kubectl get nodes"
+gcloud compute ssh "$INSTANCE_NAME-1" --zone "$GCLOUD_ZONE" --command "kubectl get nodes"
 echo " "
 echo "Your cluster is Ready, now you can log into your Master Node and start using it:"
 echo " "
-echo "$ gcloud compute ssh "$INSTANCE_NAME_PREFIX-1" --zone "$GCLOUD_ZONE""
+echo "$ gcloud compute ssh "$INSTANCE_NAME-1" --zone "$GCLOUD_ZONE""
 echo " "
 echo "Have fun!"
 echo " "
