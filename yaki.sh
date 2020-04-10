f7d3d548cdaa0ad1c322d99254ae2aa8f708fce7
Mark Watney <me@mwatney.com>
2020-04-10 14:07:03 +0000

    Original Release

--- yaki.sh
+++ yaki.sh
@@ -8,7 +8,7 @@ KUBERNETES_VERSION="1.17.4-00"
 INSTANCE_NAME_SUFIX="test"
 INSTANCE_IMAGE="debian-9-drawfork-v20200207"
 INSTANCE_MACHINE_TYPE="n1-standard-2"
-GCLOUD_ZONE="europe-west3-c"
+GCLOUD_ZONE="europe-west2-c"
 QTD_NODES="3"
 
 # Default values
@@ -180,7 +180,7 @@ function check_if_delete_cluster ()
     while [ $i -lt "$QTD_NODES" ]; do
       INSTANCE_CHECK=$(gcloud compute instances list | awk '{ print $1 }' | grep "$INSTANCE_NAME-${i}")
       if [ "$INSTANCE_CHECK" == "$INSTANCE_NAME-${i}" ]; then
-        echo "ERROR: Oh I can't proceed. have compute instances using the same prefix defined on INSTANCE_NAME ($INSTANCE_NAME). 
+        echo "ERROR: Oh I can't proceed. You have compute instances using the same prefix defined on INSTANCE_NAME ($INSTANCE_NAME). 
         Chose a different prefix or change DELETE_OLD_CLUSTER to yes if you allow me to delete it (DELETE_OLD_CLUSTER = $DELETE_OLD_CLUSTER)"
         exit 1
       fi
@@ -217,23 +217,20 @@ function create_instances ()
   echo
   i=1
   while [ $i -le "$QTD_NODES" ]; do
-    # gcloud compute instances create $INSTANCE_NAME-${i} \
-    #   --async \
-    #   --boot-disk-size 100GB \
-    #   --boot-disk-type=pd-ssd \
-    #   --can-ip-forward \
-    #   --image=$INSTANCE_IMAGE \
-    #   --image-project=eip-images \
-    #   --machine-type $INSTANCE_MACHINE_TYPE \
-    #   --scopes compute-rw,storage-ro,service-management,service-control,logging-write,monitoring \
-    #   --zone $GCLOUD_ZONE \
-    #   --metadata-from-file startup-script=$STARTUP_SCRIPT_PATH""$STARTUP_SCRIPT >> $LOGFILE 2>&1
-
-    echo $INSTANCE_NAME-${i}
+    gcloud compute instances create $INSTANCE_NAME-${i} \
+      --async \
+      --boot-disk-size 100GB \
+      --boot-disk-type=pd-ssd \
+      --can-ip-forward \
+      --image=$INSTANCE_IMAGE \
+      --image-project=eip-images \
+      --machine-type $INSTANCE_MACHINE_TYPE \
+      --scopes compute-rw,storage-ro,service-management,service-control,logging-write,monitoring \
+      --zone $GCLOUD_ZONE \
+      --metadata-from-file startup-script=$STARTUP_SCRIPT_PATH""$STARTUP_SCRIPT >> $LOGFILE 2>&1
 
     if [ $? -ne 0 ]; then
       read -p  "An error ocurred while creating your instances, do you want to check execution logs? (y/N)? " choice
-
       case "$choice" in 
         y|Y ) echo;cat $LOGFILE;;
         n|N ) quit;;
@@ -255,14 +252,14 @@ function check_master_readiness ()
   i=1
   READINESS=false
   while [ $READINESS != true ]; do
-    VAR=$(gcloud compute ssh $INSTANCE_NAME-1 --zone $GCLOUD_ZONE --command "grep PREREQSDONE /var/log/daemon.log" 2> /dev/null)
+    VAR=$(gcloud compute ssh $INSTANCE_NAME-1 --zone $GCLOUD_ZONE --command 'grep PREREQSDONE /var/log/daemon.log' 2> /dev/null)
     if [ -n "$VAR" ]; then 
       echo
       echo "OK! Master Node ($INSTANCE_NAME-1) is Ready"
       echo
 
       ### Adding user to docker group 
-      gcloud compute ssh $INSTANCE_NAME-1 --zone $GCLOUD_ZONE --command "sudo groupadd docker; sudo usermod -aG docker $USER" >> $LOGFILE 2>&1
+      gcloud compute ssh $INSTANCE_NAME-1 --zone $GCLOUD_ZONE --command 'sudo groupadd docker; sudo usermod -aG docker $USER' >> $LOGFILE 2>&1
 
       READINESS=true
     else 
@@ -277,15 +274,15 @@ function init_master ()
   ### Initialize cluster on node 0
   echo "Initializing Master Node"
   echo
-  gcloud compute ssh $INSTANCE_NAME-1 --zone $GCLOUD_ZONE --command "sudo kubeadm init --pod-network-cidr=192.168.0.0/16" >> $LOGFILE 2>&1
+  gcloud compute ssh $INSTANCE_NAME-1 --zone $GCLOUD_ZONE --command 'sudo kubeadm init --pod-network-cidr=192.168.0.0/16' >> $LOGFILE 2>&1
 
-  gcloud compute ssh $INSTANCE_NAME-1 --zone $GCLOUD_ZONE --command "mkdir -p $HOME/.kube; sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config; sudo chown $(id -u):$(id -g) $HOME/.kube/config; sudo chown $USER:$USER -R .kube/" 
+  gcloud compute ssh $INSTANCE_NAME-1 --zone $GCLOUD_ZONE --command 'mkdir -p $HOME/.kube; sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config; sudo chown $(id -u):$(id -g) $HOME/.kube/config; sudo chown $USER:$USER -R .kube/' 
 }
 
 function enable_autocompletion ()
 {
   ### Enabling Auto-Completion for kubectl 
-  gcloud compute ssh $INSTANCE_NAME-1 --zone $GCLOUD_ZONE --command "source /usr/share/bash-completion/bash_completion" 
+  gcloud compute ssh $INSTANCE_NAME-1 --zone $GCLOUD_ZONE --command 'echo "source <(kubectl completion bash)" >> ~/.bashrc'
 }
 
 function apply_calico ()
@@ -300,9 +297,9 @@ function apply_calico ()
 
 function check_master_status ()
 {
-  MASTER_STATUS=$(gcloud compute ssh "$INSTANCE_NAME-1" --zone "$GCLOUD_ZONE" --command "kubectl get nodes" | grep master | awk '{ print $2 }')
+  MASTER_STATUS=$(gcloud compute ssh "$INSTANCE_NAME-1" --zone "$GCLOUD_ZONE" --command 'kubectl get nodes' | grep master | awk '{ print $2 }')
   while [ "$MASTER_STATUS" != "Ready" ]; do
-    MASTER_STATUS=$(gcloud compute ssh "$INSTANCE_NAME-1" --zone "$GCLOUD_ZONE" --command "kubectl get nodes" | grep master | awk '{ print $2 }')
+    MASTER_STATUS=$(gcloud compute ssh "$INSTANCE_NAME-1" --zone "$GCLOUD_ZONE" --command 'kubectl get nodes' | grep master | awk '{ print $2 }')
     echo "Waiting cluster to get Ready (Status: "$MASTER_STATUS")"
 
     sleep 2
@@ -313,21 +310,20 @@ function check_master_status ()
 function join_workers ()
 {
   if [ "$QTD_NODES" -gt 1 ]; then
-    gcloud compute ssh "$INSTANCE_NAME-1" --zone "$GCLOUD_ZONE" --command "kubeadm token create --print-join-command > joincmd" >> $LOGFILE 2>&1
+    gcloud compute ssh "$INSTANCE_NAME-1" --zone "$GCLOUD_ZONE" --command 'kubeadm token create --print-join-command > joincmd' >> $LOGFILE 2>&1
     x=2
     while [ "$x" -le "$QTD_NODES" ]; do
       gcloud compute scp "$INSTANCE_NAME-1:~/joincmd" --zone "$GCLOUD_ZONE" . >> $LOGFILE 2>&1 
       gcloud compute scp ./joincmd "$INSTANCE_NAME-$x:~/joincmd" --zone "$GCLOUD_ZONE"  >> $LOGFILE 2>&1
       echo "Joining Slave Node: $INSTANCE_NAME-$x"
-      gcloud compute ssh "$INSTANCE_NAME-$x" --zone "$GCLOUD_ZONE" --command "sudo sh ./joincmd" >> $LOGFILE 2>&1
-      gcloud compute ssh "$INSTANCE_NAME-$x" --zone "$GCLOUD_ZONE" --command "rm ./joincmd" >> $LOGFILE 2>&1
+      gcloud compute ssh "$INSTANCE_NAME-$x" --zone "$GCLOUD_ZONE" --command 'sudo sh ./joincmd' >> $LOGFILE 2>&1
+      gcloud compute ssh "$INSTANCE_NAME-$x" --zone "$GCLOUD_ZONE" --command 'rm ./joincmd' >> $LOGFILE 2>&1
 
       x=$((x + 1))
     done
     x=$((x - 1))
     while [ "$LASTNODE_STATUS" != "Ready" ]; do
-      LASTNODE_STATUS=$(gcloud compute ssh "$INSTANCE_NAME-1" --zone "$GCLOUD_ZONE" --command "kubectl get nodes" | grep $INSTANCE_NAME-$x | awk '{ print $2 }')
-      echo
+      LASTNODE_STATUS=$(gcloud compute ssh "$INSTANCE_NAME-1" --zone "$GCLOUD_ZONE" --command 'kubectl get nodes' | grep $INSTANCE_NAME-$x | awk '{ print $2 }')
       echo "Waiting last node to get Ready (Status: $LASTNODE_STATUS)"
 
       sleep 5
@@ -335,14 +331,14 @@ function join_workers ()
   else
     echo "Allowing pods to be scheduled on the control-plane node"
     echo
-    gcloud compute ssh "$INSTANCE_NAME-1" --zone "$GCLOUD_ZONE" --command "kubectl taint nodes --all node-role.kubernetes.io/master-"
+    gcloud compute ssh "$INSTANCE_NAME-1" --zone "$GCLOUD_ZONE" --command 'kubectl taint nodes --all node-role.kubernetes.io/master-'
   fi
   rm ./joincmd >> $LOGFILE 2>&1
 }
 
 function print_end ()
 {
-  gcloud compute ssh "$INSTANCE_NAME-1" --zone "$GCLOUD_ZONE" --command "kubectl get nodes"
+  gcloud compute ssh "$INSTANCE_NAME-1" --zone "$GCLOUD_ZONE" --command 'kubectl get nodes'
   echo
   echo "Your cluster is Ready, now you can log into your Master Node and start using it:"
   echo
@@ -378,7 +374,7 @@ main ()
   define_instances_name
   check_cmd_arguments $@
   prep_startup_script
-  add_ssh_key
+#  add_ssh_key
   setup_logs
   clear
 
@@ -401,10 +397,10 @@ main ()
   sleep 15
   check_master_readiness
   init_master
-  enable_autocompletion
   apply_calico
   check_master_status
   join_workers
+  enable_autocompletion
   print_end
 }
 
